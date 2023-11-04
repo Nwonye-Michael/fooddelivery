5a85a52f9f220ddeeed877c9fb55cfc37e74f95f
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-11-04 10:34:32 +0100

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -65,10 +65,10 @@ function Cart() {
                 </h6>
                 <p>Taxes and shipping will calculate at checkout</p>
                 <div className="cart__page-btn">
-                  <button className="addTOCart__btn me-4">
+                  <button className="addToCart__btn me-4">
                     <Link to="/foods">Continue Shopping</Link>
                   </button>
-                  <button className="addTOCart__btn">
+                  <button className="addToCart__btn">
                     <Link to="/checkout">Proceed to checkout</Link>
                   </button>
                 </div>
